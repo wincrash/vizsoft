3161e7944818a36aa05b7b9141b608e53fac9b5c
ruslan <ruslan@sc-ruslan.vgtu.lt>
2015-05-18 14:30:36 +0300

    vislt idetas

---
+++ vislt/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+wget http://download.opensuse.org/repositories/home:/rpacevic/CentOS_CentOS-6/home:rpacevic.repo -O /etc/yum.repos.d/home:rpacevic.repo
+RUN yum -y install VisLT-osmesa
+
+if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
+    echo "=> Found authorized keys"
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+    IFS=$'\n'
+    arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
+    for x in $arr
+    do
+        x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
+        cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
+            echo "$x" >> /root/.ssh/authorized_keys
+        fi
+    done
+fi
+
+if [ ! -f /.root_pw_set ]; then
+	/set_root_pw.sh
+fi
+exec /usr/sbin/sshd -D
